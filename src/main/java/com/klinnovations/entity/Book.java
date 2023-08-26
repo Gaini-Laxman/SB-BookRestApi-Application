a80cf624f29e0a2e9c0c37e2ac87ad9f04627f4a
Gaini Laxman <gainilaxman12@gmail.com>
2023-08-26 06:57:44 +0530

    second commit

--- src/main/java/com/klinnovations/entity/Book.java
+++ src/main/java/com/klinnovations/entity/Book.java
@@ -4,10 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import lombok.Data;
 
 @Entity
-@Data
+//@Data
 public class Book {
 	
 	@Id
@@ -16,5 +15,44 @@ public class Book {
 	private Integer bookId;
 	private String bookName;
 	private Double bookPrice;
+	
+	public Book(){
+		
+	}
+	
+	public Book(Integer bookId, String bookName, Double bookPrice) {
+		super();
+		this.bookId = bookId;
+		this.bookName = bookName;
+		this.bookPrice = bookPrice;
+	}
+
+	public Integer getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(Integer bookId) {
+		this.bookId = bookId;
+	}
+
+	public String getBookName() {
+		return bookName;
+	}
+
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+
+	public Double getBookPrice() {
+		return bookPrice;
+	}
+
+	public void setBookPrice(Double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	
+	
+	
+	
 
 }
